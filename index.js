2e2b941dbad76ac55e463e9cc1f33f8097e1364a
ghoshshreya <shrghosh08@gmail.com>
2023-04-29 14:20:09 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,32 +1,61 @@
-var maxSubArray = function (nums) {
-  //BRUTE_FORCE with 3 loops :: TC - O(n^3) SC - O(1)
-  // let maxSum = nums[0];
-  // let arrayLength = nums.length;
-  // if (nums.length > 1) {
-  //   for (let x = 0; x < arrayLength; x++) {
-  //     for (let y = x; y < arrayLength; y++) {
-  //       let sum = 0;
-  //       for (let k = x; k <= y; k++) {
-  //         sum += nums[k];
-  //       }
-  //       maxSum = Math.max(maxSum, sum);
-  //     }
-  //   }
-  // }
-  
-  //BETTER APPROACH with 2 loops :: TC - O(n^2) SC - O(1)
-  // let maxSum = nums[0];
-  // const arrLength = nums.length;
-  // if (arrLength > 1) {
-  //   for (let x = 0; x < arrLength; x++) {
-  //     let sum = nums[x];
-  //     for (let y = x + 1; y < arrLength; y++) {
-  //       sum += nums[y];
-  //       maxSum = Math.max(maxSum, sum);
-  //     }
-  //   }
-  // }
-  // return maxSum;
+// https://leetcode.com/problems/maximum-subarray/submissions/941457409/
+
+//BRUTE_FORCE with 3 loops :: TC - O(n^3) SC - O(1)
+var bruteForceSolution = function () {
+  let maxSum = this.nums[0];
+  let arrayLength = this.nums.length;
+  if (this.nums.length > 1) {
+    for (let x = 0; x < arrayLength; x++) {
+      for (let y = x; y < arrayLength; y++) {
+        let sum = 0;
+        for (let k = x; k <= y; k++) {
+          sum += this.nums[k];
+        }
+        maxSum = Math.max(maxSum, sum);
+      }
+    }
+  }
+  return maxSum;
 };
 
-console.log(maxSubArray([-2, -3, 4, -1, -2, 1, 5, -3]));
+//BETTER APPROACH with 2 loops :: TC - O(n^2) SC - O(1)
+var betterSolution = function () {
+  let maxSum = this.nums[0];
+  const arrLength = this.nums.length;
+  if (arrLength > 1) {
+    for (let x = 0; x < arrLength; x++) {
+      let sum = this.nums[x];
+      for (let y = x + 1; y < arrLength; y++) {
+        sum += this.nums[y];
+        maxSum = Math.max(maxSum, sum);
+      }
+    }
+  }
+  return maxSum;
+};
+
+// OPTIMAL SOLUTION :: KADANE'S ALGORITH :: TC - O(n) SC - O(1)
+var kadanesSolution = function () {
+  let maxSum = -Infinity;
+  let sum = 0;
+  if (this.nums.length) {
+    this.nums.forEach((x) => {
+      sum += x;
+      maxSum = Math.max(sum, maxSum);
+      if (sum < 0) sum = 0;
+    });
+  }
+  return maxSum;
+};
+
+var maxSubArray = function (nums, type) {
+  return type.call(nums);
+};
+
+const inputArray = {
+  nums: [-2, -3, 4, -1, -2, 1, 5, -3],
+};
+
+console.log(maxSubArray(inputArray, bruteForceSolution));
+console.log(maxSubArray(inputArray, betterSolution));
+console.log(maxSubArray(inputArray, kadanesSolution));
